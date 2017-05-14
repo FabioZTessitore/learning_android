48b274e9583b4c075a3cdda8170b22ac8435334a
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-05-14 08:43:53 +0200

    My First App

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirspapp.Message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+
+        EditText textView = (EditText) findViewById(R.id.editText);
+        String message = textView.getText().toString();
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
