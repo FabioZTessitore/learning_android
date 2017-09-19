700cb8591c32f6b6ecbd89ffa83e677344c738a6
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-09-19 12:15:33 +0200

    widgets, checkbox

--- P003_widgets/app/src/main/java/com/fzt/learning_android/p003_widgets/CheckBoxActivity.java
+++ P003_widgets/app/src/main/java/com/fzt/learning_android/p003_widgets/CheckBoxActivity.java
@@ -1,7 +1,10 @@
 package com.fzt.learning_android.p003_widgets;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class CheckBoxActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class CheckBoxActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_box);
+
+        this.updateCheckbox();
+    }
+
+    private void updateCheckbox() {
+        CheckBox cb = (CheckBox)findViewById(R.id.checkBox);
+
+        boolean toggleStatus = cb.isChecked();
+
+        cb.setText(toggleStatus ? R.string.cb_toggle_on : R.string.cb_toggle_off);
+    }
+
+    public void onToggle(View view) {
+        this.updateCheckbox();
+    }
+
+    public void prev(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
