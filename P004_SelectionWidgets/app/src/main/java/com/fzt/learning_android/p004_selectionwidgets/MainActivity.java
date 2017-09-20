3d1bdc2822e1d605f475fc01b8e72be8991314b1
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-09-20 21:05:19 +0200

    selectionwidgets, listview

---
+++ P004_SelectionWidgets/app/src/main/java/com/fzt/learning_android/p004_selectionwidgets/MainActivity.java
@@ -0,0 +1,26 @@
+package com.fzt.learning_android.p004_selectionwidgets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] items={ "lorem", "ipsum", "dolor", "sit", "amet",
+        "consectetuer", "adipiscing", "elit", "morbi", "vel",
+        "ligula", "vitae", "arcu", "aliquet", "mollis",
+        "etiam", "vel", "erat", "placerat", "ante",
+        "porttitor", "sodales", "pellentesque", "augue", "purus"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(arrayAdapter);
+    }
+}
