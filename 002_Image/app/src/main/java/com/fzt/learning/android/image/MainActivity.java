ac0ecee2178d0df65c5633f0b5b587b9dffe65e4
Fabio Z Tessitore <fabioztessitore@gmail.com>
2018-05-08 12:21:37 +0200

    image

---
+++ 002_Image/app/src/main/java/com/fzt/learning/android/image/MainActivity.java
@@ -0,0 +1,21 @@
+package com.fzt.learning.android.image;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* Visualizzare un'immagine:
+
+        copia/incolla l'immagine in res.drawable
+
+        aggiungere una ImageView e selezionare
+        @drawable/ferrari a src
+         */
+    }
+}
