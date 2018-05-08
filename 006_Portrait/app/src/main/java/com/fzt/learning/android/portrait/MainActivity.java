89449281d7b62431fc3e88363436eb5b6ef0564e
Fabio Z Tessitore <fabioztessitore@gmail.com>
2018-05-08 22:58:45 +0200

    force portrait

---
+++ 006_Portrait/app/src/main/java/com/fzt/learning/android/portrait/MainActivity.java
@@ -0,0 +1,18 @@
+package com.fzt.learning.android.portrait;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* Forza l'attivita' alla visualizzazione
+        portrait modificando la screenOrientation
+        dell'activity nel AndroidMmanifest.xml
+        */
+    }
+}
