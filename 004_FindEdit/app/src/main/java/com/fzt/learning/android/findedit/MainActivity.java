5eb27126c3cfeb7c3f0398ea91c698c2abdfc0bf
Fabio Z Tessitore <fabioztessitore@gmail.com>
2018-05-08 15:30:41 +0200

    find and edit a textview

---
+++ 004_FindEdit/app/src/main/java/com/fzt/learning/android/findedit/MainActivity.java
@@ -0,0 +1,27 @@
+package com.fzt.learning.android.findedit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* Dopo aver creato una stringa nelle risorse
+        modifichiamo il testo della TextView.
+
+        Preliminarmente viene modificato l'ID
+        della TextView in "welcomeTextView"
+         */
+
+        // seleziona la TextView
+        TextView welcomeTextView = (TextView) findViewById(R.id.welcomeTextView);
+
+        // modifica il testo
+        welcomeTextView.setText(R.string.welcome);
+    }
+}
