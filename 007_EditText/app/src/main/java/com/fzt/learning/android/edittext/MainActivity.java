38dc44b81c17ebd01f26101db60453ea0a31d28b
Fabio Z Tessitore <fabioztessitore@gmail.com>
2018-05-08 23:31:52 +0200

    edittext

---
+++ 007_EditText/app/src/main/java/com/fzt/learning/android/edittext/MainActivity.java
@@ -0,0 +1,40 @@
+package com.fzt.learning.android.edittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView messageTextView;
+    private EditText messageEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        messageTextView = (TextView) findViewById(R.id.messageTextView);
+        messageEditText = (EditText) findViewById(R.id.messageEditText);
+
+        messageEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                messageTextView.setText(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+}
